42b699aa8994673604b95e433a6d388d515d411b
Derek <49753517+equitymarkets@users.noreply.github.com>
2022-09-08 11:31:42 -0400

    Create isogram.cpp

---
+++ isogram.cpp
@@ -0,0 +1,30 @@
+/*
+An isogram is a word that has no repeating letters, 
+consecutive or non-consecutive. Implement a function 
+that determines whether a string that contains only 
+letters is an isogram. Assume the empty string is an isogram. 
+Ignore letter case.
+*/
+
+#include <iostream>
+#include <cstring>
+
+bool is_isogram(std::string str) 
+   {
+   int size = str.length();
+   char arr[size];
+   int i,j;
+   strcpy(arr,str.c_str());
+   
+   for(i = 0; i < size; i++)
+      {
+         if(i > 0)
+         {
+            for(j = i - 1; j >= 0; j--)
+               {
+               if(str[i] == str[j]) return false;
+               }
+         }
+      }
+   return true;
+}
