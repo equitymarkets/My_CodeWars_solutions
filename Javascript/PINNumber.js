c546f203b15437992ec3b7e4a638133c1486c9a4
Derek <49753517+equitymarkets@users.noreply.github.com>
2022-10-20 19:14:17 -0400

    Create PINNumber.js

---
+++ Javascript/PINNumber.js
@@ -0,0 +1,15 @@
+/*
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+
+Examples (Input --> Output)
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+*/
+
+function validatePIN (pin) {
+  return ((pin.length === 4 || pin.length === 6) 
+         && pin.match(/^[0-9]+$/) ? true : false)
+}
