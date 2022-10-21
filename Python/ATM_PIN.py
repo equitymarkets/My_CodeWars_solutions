fa6d61b6c713c6fbb3f669d332e87f33f9d9539f
Derek <49753517+equitymarkets@users.noreply.github.com>
2022-10-21 13:12:28 -0400

    Create ATM_PIN.py

---
+++ Python/ATM_PIN.py
@@ -0,0 +1,25 @@
+"""
+ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+If the function is passed a valid PIN string, return true, else return false.
+
+Examples (Input --> Output)
+"1234"   -->  true
+"12345"  -->  false
+"a234"   -->  false
+"""
+import re
+
+def validate_pin(pin):
+    num_validator = "^\\d+$"
+    space_check = ' ' in pin
+    if(space_check == True): 
+        return False
+    return_check = '\n' in pin      #check for newlines not just spaces!
+    if(return_check == True):
+        return False
+    length_str_pin = len(pin)
+    if((length_str_pin == 4 or length_str_pin == 6) and (re.match(num_validator, pin))): 
+        return True
+    else: 
+        return False
